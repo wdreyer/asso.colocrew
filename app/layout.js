d8a8e25582444728969f3b7375f7ee6e21e57520
Your name <dreyer.wil@gmail.com>
2024-09-28 21:45:21 +0300

    4nd commit

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,20 @@
 // app/layout.js
 import './globals.css';
 
+export const metadata = {
+  title: 'Colocrew',
+  description: 'Des colos réinventés',
+  icons: {
+    icon: '/favicon.ico',  // Assurez-vous que votre favicon.ico est bien dans le dossier public
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <head />
+      <head>
+        {/* Le contenu des balises <head> sera géré ici automatiquement par Next.js */}
+      </head>
       <body>{children}</body>
     </html>
   );
