7012d20fbeac7bb4e790192030309f7d22003cd0
Your name <dreyer.wil@gmail.com>
2025-01-29 18:31:19 -0500

    analitycs

--- app/layout.js
+++ app/layout.js
@@ -1,38 +1,48 @@
 // app/layout.js
 import './globals.css';
-import Script from 'next/script'
+import Script from 'next/script';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-
 
 export const metadata = {
   title: 'Colocrew',
   description: 'Des colos réinventés',
-
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
-        {/* Le contenu des balises <head> sera géré ici automatiquement par Next.js */} 
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-PWJ8EQLG1Z"></Script>
-
+        {/* Script GA principal */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PWJ8EQLG1Z"
+          strategy="afterInteractive"
+        />
+        
+        {/* Script de configuration GA */}
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PWJ8EQLG1Z');
+          `}
+        </Script>
       </head>
       <body>
-      <div className="flex flex-col min-h-screen">
-      
-      <Header />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-      </div>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+        
+        {/* Script SimpleAnalytics */}
+        <Script
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+          strategy="afterInteractive"
+        />
       </body>
-      <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
-
     </html>
   );
 }
-
-
-// app/layout.js
