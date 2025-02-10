c1bd89d841efe7ed7f54b3f2c9c3f5cc154c9daa
Your name <dreyer.wil@gmail.com>
2025-02-09 20:18:34 -0500

    big maj

--- app/api/reservation/route.js
+++ app/api/reservation/route.js
@@ -149,7 +149,7 @@ export async function POST(request) {
     const lienAcces = `colocrew.com/reservation/${tokenUnique}`;
 
     // 3) Appeler l'envoi d'email
-    const sendMailRes = await fetch("colocrew.com/api/send-email", {
+    const sendMailRes = await fetch(`${baseUrl}/api/send-email`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
