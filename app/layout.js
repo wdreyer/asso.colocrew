3b48b85ba83dec627b1796171e3cf545cb837f44
Your name <dreyer.wil@gmail.com>
2026-01-09 15:24:50 +0100

    favicon

--- app/layout.js
+++ app/layout.js
@@ -40,7 +40,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         {/* Google Tag Manager */}
         <Script
           id="gtm-base"
